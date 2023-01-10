6158b11c5607fae2fce9bdcf6a7e1172c287a592
hwandev12 <husan.ahmedov777@yandex.com>
2023-01-10 10:38:53 +0500

    running some test

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import FacebookPost
 
-# Create your tests here.
+class FacebookPostTestCase(TestCase):
+    
+    def setUp(self):
+        FacebookPost.objects.create(post_text='This is tested text')
+        
+    def test_facebook_post_accurate(self):
+        """Should check any inner or model error here is occured!"""
+        post = FacebookPost.objects.get(post_text='This is tested text')
+        self.assertEqual(post, 'This is tested text')
+
