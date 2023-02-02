a8e37f19436b69e4522ca8712b2bb076d04531e1
hwandev12 <husan.ahmedov777@yandex.com>
2023-02-02 14:00:24 +0500

    document api

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,11 @@ from django.contrib.auth.views import (
 
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
+
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title="Blog API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +30,6 @@ urlpatterns = [
     re_path(r'^api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
     re_path(r'^logout/$', LogoutView.as_view(), name='logout'),
     re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+    path('docs/', include_docs_urls(title="Blog API")),
+    path('schema/', schema_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
